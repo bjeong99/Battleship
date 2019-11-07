08b0dc5a32bf96d36febb0ffe96838f144831dd9
bj254 <bj254@cornell.edu>
2019-11-06 23:18:30 -0500

    Edited bs.ml and bs.mli deleted bs2.ml

--- battleship.mli
+++ battleship.mli
@@ -4,9 +4,8 @@ type direction =
   | Up
   | Down
 type status = 
-  | Unknown 
-  | Hit
-  | Miss
+  | Damaged
+  | Undamaged
 type ship_type = 
   | Battleship 
   | AircraftCarrier 
@@ -21,4 +20,12 @@ val empty: t
 val ship_length: ship_type -> int
 val get_ship_list: t -> string list -> string list
 val get_ship_from_coord: int -> int -> t -> string 
-val generate_post_list ()
+val generate_pos_list: int * int -> int -> direction -> (int * int * status) list
+val string_to_ship: string -> ship_type
+val insert: string -> int * int -> direction -> t -> t
+val remove: string -> t -> t
+val check_bounds: int * int -> int -> direction -> bool
+val check_cell_occupied: int * int -> t -> bool
+val check_unoccupied: int * int -> int -> direction -> t -> bool
+val empty_board: string array array
+val make_grid: t -> string array array -> string array array
