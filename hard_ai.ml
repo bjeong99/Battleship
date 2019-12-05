e26d9f277d16b197dc68d6bb834abfe4a3c2a89a
Jonathan Tran <jtran2718@gmail.com>
2019-12-05 02:50:39 -0500

    updated design doc with hard AI

--- hard_ai.ml
+++ hard_ai.ml
@@ -124,23 +124,3 @@ let smart_to_random ai ship_sunk =
      vertical_points = [];}
   else ai
 
-
-
-
-
-(*
-The Hard AI works in this manner:
-It has a guess stage where it goes across the board, 
-  THE GUESS PROCEDURE:
-  Randomly Guess 
-  If it hits a ship, in which case it enters the hit stage, 
-  store the horizontal and vertical tiles around the ship
-
-The hit stage works like this:
-  If the ship is sunk, go back in the guess procedure
-  Otherwise choose horizontal targeting 
-    - if there is no hit on either horizontal side 
-      - switch to a vertical target and accumulate vertical targets
-    - continue until the ship is sunk
-
-*)
