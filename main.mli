3af2f8a575decb2df16a25eac3d09f82c302ffdb
bj254 <bj254@cornell.edu>
2019-12-05 20:42:50 -0500

    Edited .mli files

--- main.mli
+++
@@ -1,8 +0,0 @@
-(*
-type name = string
-
-val print_game_board : name -> Battleship.t -> unit
-val get_name : unit -> name
-val pregame_setup : unit -> Battleship.t
-val play : Battleship.t -> State.t -> unit 
-*)
