0553c402f80ca7c06c6f95d87845b9dc6287f8cd
Jonathan Tran <jtran2718@gmail.com>
2019-12-11 18:17:07 -0500

    delete all unused files

--- bad_ai.mli
+++
@@ -1,6 +0,0 @@
-(* type t
-
-   val initialize_ai : unit -> t
-   val create_pairs : int -> int -> (int * int) list
-   val target : t -> ((int * int) * t)
-   val randomly_laydown_ships : (int * int) list -> (int * int) * int * int *)
